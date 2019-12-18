d6356391924f5cc271d8de69629f4175a10cff81
Pasha <Pavel.Arashkou@itechart-group.com>
2019-12-18 13:38:59 +0300

    added titles component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Titles from "./components/Titles";
+
 
 class App extends React.Component {
   render() {
     return (
       <div> 
-        <p>Hello!</p>
-        <h1>YO!</h1>
+        <Titles />
       </div>
     );
   }
