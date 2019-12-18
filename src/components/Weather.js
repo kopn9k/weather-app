42ddf76a74be7f40ca71c03ff6dc0b58c51d09dc
Pasha <Pavel.Arashkou@itechart-group.com>
2019-12-18 13:45:00 +0300

    added Weather component

---
+++ src/components/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+class Weather extends React.Component {
+    render() {
+        return(
+            <div>Weather Component</div>
+        ); 
+    }
+}
+
+export default Weather;
