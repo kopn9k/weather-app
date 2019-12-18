d6a01ed90a719f0d16ece686316abc20e5c81797
Pasha <Pavel.Arashkou@itechart-group.com>
2019-12-18 15:44:58 +0300

    added functionality showing data from the api call

--- src/App.js
+++ src/App.js
@@ -12,15 +12,46 @@ const DEFAULT_UNITS = "metric";
 
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
 
   getWeather = async (e) => {
     e.preventDefault();
+
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
+
     const api_call = await fetch(`${WETHER_API_URL}?${URL_PARAMETER_LOCATION}=${city},${country}
       &${URL_PARAMETER_APPID}=${API_KEY}&${URL_PARAMETER_UNITS}=${DEFAULT_UNITS}`);
     const data = await api_call.json();
-    console.log(data);
+
+    if (city && country) {
+      console.log(data);
+
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the values :)"
+      });
+    }
   }
 
   render() {
@@ -28,7 +59,12 @@ class App extends React.Component {
       <div> 
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather temperature = {this.state.temperature}
+          city = {this.state.city}
+          country = {this.state.country}
+          humidity = {this.state.humidity}
+          description = {this.state.description}
+          error = {this.state.error}/>
       </div>
     );
   }
