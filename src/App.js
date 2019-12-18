81a91f0dbc27197fef868986e14ae8344b8df2bd
Pasha <Pavel.Arashkou@itechart-group.com>
2019-12-18 13:41:33 +0300

    added Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Titles from "./components/Titles";
+import Form from "./components/Form";
 
 
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
     return (
       <div> 
         <Titles />
+        <Form />
       </div>
     );
   }
